7a2ca05059f86bf87dc8cc057627913d23f29b73
Jamesford <jamie@stobbs.me>
2020-12-12 18:09:13 +0100

    remove setting page title fron env var

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,10 @@
 import Head from 'next/head'
-const { NEXT_PUBLIC_PAGE_TITLE } = process.env
 
 export default function Layout({ withZxcvbn = false, children }) {
   return (
     <>
       <Head>
-        <title>{NEXT_PUBLIC_PAGE_TITLE}</title>
+        <title>Cryptshare</title>
 
         {withZxcvbn && (
           <script async type="text/javascript" src="/static/zxcvbn.js" />
